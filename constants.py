0169c08a5a6c84623b5822b18bc9dc1305adc5c8
RenzoCardoza <car22001@byui.edu>
2022-07-05 17:36:25 -0300

    constants file

---
+++ constants.py
@@ -0,0 +1,22 @@
+from game.shared.color import Color
+
+#GAME
+GAME_NAME = "Space Invaders"
+DEFAULT_LIVES = 3
+FRAME_RATE = 45
+
+#SCREEN 
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+CENTER_X = SCREEN_WIDTH / 2
+CENTER_Y = SCREEN_HEIGHT / 2
+COLUMNS = 40 
+ROWS = 20
+CELL_SIZE = 15
+FONT_SIZE = 15
+
+#COLORS
+BLACK = Color(0, 0, 0)
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+BLUE = Color(0, 0, 255)
