d8ecf13ab3fb58baae6d78fb7cf90716a9246c72
RenzoCardoza <car22001@byui.edu>
2022-07-05 22:16:34 -0300

    constants updated

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 from game.shared.color import Color
+from game.shared.point import Point
 
 #GAME
 GAME_NAME = "Space Invaders"
@@ -10,8 +11,8 @@ SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
 CENTER_X = SCREEN_WIDTH / 2
 CENTER_Y = SCREEN_HEIGHT / 2
-COLUMNS = 40 
-ROWS = 20
+COLUMNS = 60 
+ROWS = 40
 CELL_SIZE = 15
 FONT_SIZE = 15
 
@@ -20,3 +21,8 @@ BLACK = Color(0, 0, 0)
 WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
 BLUE = Color(0, 0, 255)
+
+#ENEMIES
+DEFAULT_ARTIFACTS = 70
+ENEMIES = ["*", "o",]
+ENEMIES_VELOCITY = Point(0, 5)
