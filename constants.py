27ddfa2ff2bc0bfbf969de3e3bc72145bee165eb
Pangloss1107 <97195535+Pangloss1107@users.noreply.github.com>
2022-07-05 21:22:54 -0300

    constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+from game.shared.color import Color
+from game.shared.point import Point
+
+
+FRAME_RATE = 12
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+CELL_SIZE = 25
+FONT_SIZE = 25
+COLS = 60
+ROWS = 40
+CAPTION = "GALAGA"
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+YELLOW = Color(255, 255, 0)
+GREEN = Color(0, 255, 0)
+DEFAULT_ARTIFACTS = 70
+ENEMIES = ["*", "o",]
+ENEMIES_VELOCITY = Point(0, 5)
