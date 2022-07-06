ea5added82270d62b1d3323f286a5665db9674fd
RenzoCardoza <car22001@byui.edu>
2022-07-05 23:06:16 -0300

    spacecraft

--- game/casting/spacecraft.py
+++ game/casting/spacecraft.py
@@ -1,54 +1,24 @@
 import constants
 from game.casting.actor import Actor
 from game.shared.point import Point
-from game.shared.color import Color
-from game.casting.cast import Cast
-#from scripting.move_actors_action import MoveActorsAction
 
 class Spacecraft(Actor):
-    """An artifact in this game could be a rock or a gem
+    """A spacecraft in this game is the main player
     
-    The responsability of the artifact is to keep track of the 
-    score earned by the player
+    The responsability of the spacecraft is to move itself in 
+    the screen avoinding lasers to survive
     
     Attributes:
-        score (int) = the score earned depending if the character is a gem or a rock
     """
     
     def __init__(self):
         """construct a new instance of an artifact"""   
         super().__init__()
-        self._spacecraft = "#" 
-        self._position = ""
-        self._spacecraftcolor = constants.WHITE
+        self._color = constants.WHITE
     
     def get_player(self):
 
-        #Creating the spacecraft
-        player = Actor()
-        player.set_color(constants.WHITE)
-        player.set_position(constants.position)
-        player.set_font_size(constants.FONT_SIZE)
-        player.set_text(self._spacecraft)
-        cast = Cast()
-        cast.add_actor("players", player)
+        return self.get_position()
     
-    def prepare_player(self):
-        """Prepare the player to be drawn"""
-        
-        x = int(constants.SCREEN_HEIGHT )
-        y = int(constants.SCREEN_WIDTH )
 
-        position = Point(x,y)
-        velocity = Point(constants.CELL_SIZE, 0)
-
-        spacecraft = Actor()
-        spacecraft.set_position(position)
-        spacecraft.set_velocity(velocity)
-        spacecraft.set_text(self._spacecraft)
-        spacecraft.set_color(self._spacecraftcolor)
-
-
-    def move_next(self):
-        """Moves the player"""
-        return self._spacecraft.move_next()
+    #def reset()
