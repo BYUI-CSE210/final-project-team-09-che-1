e352f5f339f4e75ce9adc5e44e064553725533df
Pangloss1107 <97195535+Pangloss1107@users.noreply.github.com>
2022-07-08 00:02:38 -0300

    Control_laser class added

---
+++ game/scripting/control_laser_action.py
@@ -0,0 +1,46 @@
+import pyray
+import constants
+from game.casting.laser import Laser
+from game.scripting.action import Action
+from game.shared.point import Point
+#from game.scripting.handle_colission_action import HandleCollisionsAction
+
+class ControlLaserAction(Action):
+    """
+    An input action that controls the spacecraft's movement.
+    
+    The responsibility of ControlActorsAction is to get the direction and move the spacecraft.
+    Attributes:
+        _keyboard_service (KeyboardService): An instance of KeyboardService.
+        _direction (Point): a point to direct the spacecraft
+    """
+
+    def __init__(self, keyboard_service):
+        """Constructs a new ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+
+        self._keyboard_service = keyboard_service
+        self._direction = Point(constants.CELL_SIZE, 0)
+
+    def execute(self, cast, script):
+        """Executes the control actors action.
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+
+        dx = 0
+        dy = 0
+
+        if self._keyboard_service.is_key_down("f"):
+            dy = -1
+        
+        
+        self._direction = Point(dx, dy)
+        self._direction = self._direction.scale(constants.CELL_SIZE)
+        laser = cast.get_first_actor("laser")
+        laser.set_velocity(self._direction) 
+    
