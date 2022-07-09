78d1767ca4a79e9cb2e536aea765356795f661b5
RenzoCardoza <car22001@byui.edu>
2022-07-08 21:11:15 -0300

    lives and draw hud actions

--- game/scripting/draw_actors_action.py
+++ game/scripting/draw_actors_action.py
@@ -26,6 +26,8 @@ class DrawActorsAction(Action):
         background = cast.get_actors("background")
         enemy = cast.get_actors("enemies")
         laser = cast.get_actors("lasers")
+        score = cast.get_first_actor("scores")
+        lives = cast.get_first_actor("lives")
 
 
         self._video_service.clear_buffer()
@@ -33,4 +35,6 @@ class DrawActorsAction(Action):
         self._video_service.draw_actors(background)
         self._video_service.draw_actors(enemy)
         self._video_service.draw_actors(laser)
+        self._video_service.draw_actor(score)
+        self._video_service.draw_actor(lives)
         self._video_service.flush_buffer()
